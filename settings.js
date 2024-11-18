11823a1f77749eddae08474ee408db4efcfd7d1b
Cypher-Ilerioluwa <168092754+Cypher-Ilerioluwa@users.noreply.github.com>
2024-11-18 10:51:26 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
   packname: 'My Sticker Pack',
   author: 'My Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "ÀKÀNDÉ-MIDÉ-BOT🤖🌹🍀",
+  botOwner: 'Cypher Ìléríolúwa', // Your name
+  ownerNumber: '2349121881343', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
